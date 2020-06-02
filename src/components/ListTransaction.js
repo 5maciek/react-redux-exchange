c84e1f06d7566fdc832f7c766dd136e350f4f393
5maciek <smaciek1@op.pl>
2020-06-02 15:48:03 +0200

    add list transactions component with styles

---
+++ src/components/ListTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemTransaction from './ItemTransaction.js'
+import './ListTransaction.scss'
+
+function ListTransaction() {
+    return (
+        <div className="listTransaction">
+           <ItemTransaction />
+        </div>
+    );
+}
+
+export default ListTransaction;
