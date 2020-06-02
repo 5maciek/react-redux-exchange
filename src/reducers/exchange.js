a029f875e83739e76aac441dc6c13ea2a6eb6e8c
5maciek <smaciek1@op.pl>
2020-06-02 21:29:32 +0200

    add initial state and handle add transaction

--- src/reducers/exchange.js
+++ src/reducers/exchange.js
@@ -1,9 +1,14 @@
-export const exchange = (state = [], action) => {
+const INITIAL_STATE = {
+  currentCurrencyValue: 4.2,
+  transactions: [],
+};
+
+export const exchange = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'ADD_TRANSACTION':
-
+      return { ...state, transactions: [...state.transactions, action.item] };
     case 'REMOVE_TRANSACTION':
-
+      return { ...state, transactions: [...state.transactions, action.item] };
     default:
       return state;
   }
