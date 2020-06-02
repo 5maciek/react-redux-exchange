d7eb88f347a8e19cd7dab018dc55eb82ac2bcdb5
5maciek <smaciek1@op.pl>
2020-06-02 15:48:17 +0200

    add item transaction component with styles

---
+++ src/components/ItemTransaction.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import './ListTransaction.scss'
+
+function ItemTransaction() {
+    return (
+        <>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>     
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>              
+
+          <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>              
+
+          <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>              
+
+          <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>
+        <li key="1" className="ItemTransaction">
+            <span>1.</span>
+            <p className="name">Tranzakcja 1</p>
+            <p>Euro <span>1123</span></p>
+            <p>Pln <span>11123</span></p>
+            <button>X</button>
+        </li>              
+                 
+        
+      
+        
+        </>
+    );
+}
+
+export default ItemTransaction;
