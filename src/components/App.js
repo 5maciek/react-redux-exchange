84cfcfa9386ca7b37881e60260a7007da71f70ef
5maciek <smaciek1@op.pl>
2020-06-02 15:48:56 +0200

    Add App component with styles

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CurrentValue from './CurrentValue'
+import AddTransaction from './AddTransaction'
+import HighTransaction from './HighTransaction'
+import ListTransaction from './ListTransaction'
+import TotalTransaction from './TotalTransaction'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <CurrentValue />
+        <AddTransaction />
+        <HighTransaction />
+      </header>
+      <main>
+        <ListTransaction />
+      </main>
+      <footer>
+        <TotalTransaction />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
