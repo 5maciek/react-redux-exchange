1cc046e781ee258f49576603ff5d87fec4847d07
5maciek <smaciek1@op.pl>
2020-06-02 15:46:56 +0200

    add current value component with styles

---
+++ src/components/CurrentValue.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CurrentValue.scss'
+
+function CurrentValue() {
+  return (
+    <div className="setValues">
+      <h2>Set current value</h2>
+      <div className="form">
+        <label for="currencyValue">1 Euro = </label>
+        <input type='number' id="currencyValue"  required min ="0" step='0.01' placeholder='0.00' />
+        <p>PLN</p>
+      </div>
+      <button className="save">Save</button>
+    </div>
+  );
+}
+
+export default CurrentValue;
