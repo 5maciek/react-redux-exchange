d2ca4fce45a8e3536054f5a0e4482ad8175d1a5e
5maciek <smaciek1@op.pl>
2020-06-02 21:32:26 +0200

    connect to store and send data to Item component

--- src/components/ListTransaction.js
+++ src/components/ListTransaction.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ItemTransaction from './ItemTransaction.js'
-import './ListTransaction.scss'
+import ItemTransaction from './ItemTransaction.js';
+import { connect } from 'react-redux';
+import './ListTransaction.scss';
 
-function ListTransaction() {
-    return (
-        <div className="listTransaction">
-           <ItemTransaction />
-        </div>
-    );
+function ListTransaction(props) {
+  const { transactions } = props.items;
+  console.log('tr', transactions);
+  return (
+    <div className="listTransaction">
+      <ul>
+        {transactions.map((item, index) => (
+          <ItemTransaction item={item} index={index} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ListTransaction;
+const mapStateToProps = (state) => {
+  return {
+    items: state,
+  };
+};
+
+export default connect(mapStateToProps, null)(ListTransaction);
