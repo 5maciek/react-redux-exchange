2c9155c5be54d103c10f8416d79730f172003570
5maciek <smaciek1@op.pl>
2020-06-02 15:47:44 +0200

    add total transactions component with styles

---
+++ src/components/TotalTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TotalTransaction.scss'
+
+function TotalTransaction() {
+    return (
+        <div className="totalTransaction">          
+          <p>TOTAL Euro<span>123</span></p>
+          <p>TOTAL PLN<span>4567</span></p>
+        </div>
+    );
+}
+
+export default TotalTransaction;
