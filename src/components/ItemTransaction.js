a918b7b9a665e07f6612454e2baf014cde2f7bff
5maciek <smaciek1@op.pl>
2020-06-03 08:58:10 +0200

    handle click remove on Item transaction

--- src/components/ItemTransaction.js
+++ src/components/ItemTransaction.js
@@ -4,7 +4,7 @@ import './ListTransaction.scss';
 function ItemTransaction(props) {
   const { id, name, euroAmount, plnAmount } = props.item;
   return (
-    <li key="{id}" className="ItemTransaction">
+    <li className="ItemTransaction">
       <span>{props.index + 1}</span>
       <p className="name">{name}</p>
       <p>
@@ -13,7 +13,7 @@ function ItemTransaction(props) {
       <p>
         Pln <span>{plnAmount}</span>
       </p>
-      <button>X</button>
+      <button onClick={() => props.handleRemove(id)}>X</button>
     </li>
   );
 }
