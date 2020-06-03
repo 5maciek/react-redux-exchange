7e2049661da445a23e0094b7089d1c773e5f19db
5maciek <smaciek1@op.pl>
2020-06-03 09:55:07 +0200

    handle display total transactions amounts

--- src/components/TotalTransaction.js
+++ src/components/TotalTransaction.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './TotalTransaction.scss'
 
-function TotalTransaction() {
-    return (
-        <div className="totalTransaction">          
-          <p>TOTAL Euro<span>123</span></p>
-          <p>TOTAL PLN<span>4567</span></p>
-        </div>
-    );
+function TotalTransaction(props) {
+  const { transactions, currentCurrencyValue } = props.items;
+  console.log(props.items);
+
+  const totalTransactionsInEuro = transactions.reduce((total, item) => total += item.euroAmount, 0).toFixed(2);
+  const totalTransactionsInPLN = (totalTransactionsInEuro * currentCurrencyValue).toFixed(2);
+
+
+  return (
+    <div className="totalTransaction">
+      <p>TOTAL Euro:<span>{totalTransactionsInEuro}</span></p>
+      <p>TOTAL PLN:<span>{totalTransactionsInPLN}</span></p>
+    </div>
+  );
 }
 
-export default TotalTransaction;
+const mapStateToProps = (state) => {
+  return {
+    items: state,
+  };
+};
+
+export default connect(mapStateToProps, null)(TotalTransaction);
