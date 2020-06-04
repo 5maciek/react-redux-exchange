7bf5737a3a7534ee9c30af13fcfd11a7b88b5c40
5maciek <smaciek1@op.pl>
2020-06-04 21:29:27 +0200

    fix bug with add items

--- src/components/Main/ItemTransaction.js
+++ src/components/Main/ItemTransaction.js
@@ -3,7 +3,6 @@ import React from 'react';
 function ItemTransaction(props) {
   const { id, name, euroAmount, plnAmount } = props.transaction;
   return (
-    <>
     <li className="ItemTransaction">
       <span>{props.index + 1}.</span>
       <p className="name">{name}</p>
@@ -15,75 +14,6 @@ function ItemTransaction(props) {
       </p>
       <button onClick={() => props.handleRemove(id)}>X</button>
     </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    <li className="ItemTransaction">
-      <span>{props.index + 1}.</span>
-      <p className="name">{name}</p>
-      <p className="amount">
-        Euro:<span>{euroAmount}</span>
-      </p>
-      <p className="amount">
-        Pln:<span>{plnAmount}</span>
-      </p>
-      <button onClick={() => props.handleRemove(id)}>X</button>
-    </li>
-    
-    
-    </>
   );
 }
 
